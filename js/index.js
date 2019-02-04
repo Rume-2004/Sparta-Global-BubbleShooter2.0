7f4e08989737664087afb2935d9d743550bf5da8
Rume Rhuada <rumerhuada@spartaglobal.com>
2019-02-04 14:26:24 +0000

    I have added a background for the board

---
+++ js/index.js
@@ -0,0 +1,4 @@
+console.log("Hi it's working");
+alert("How to Play? When three or more bubbles of the same colour connect. The bubbles will pop. Use your mouse to connect bubbles of the same colour. Good Luck")
+
+var score = document.getElementsByTagName('h3')
