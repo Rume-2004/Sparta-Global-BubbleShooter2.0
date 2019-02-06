bbb924a30e10e93e713097d08be95f5bddb6da72
Rume Rhuada <rumerhuada@spartaglobal.com>
2019-02-06 11:19:56 +0000

    I have altered the pics to fit the size for the board

--- js/index.js
+++ js/index.js
@@ -21,7 +21,8 @@ var green = document.getElementsByClassName('green')
 var clear = document.getElementsByClassName('clear')
 
 for (var y = 0; y < mapArray.length; y++) {
-  for (var x = 0; x < mapArray[y].length; x++) {
+  for (var x = 0; x < mapArray[y].length; x++)
+   {
     console.log(y,x);
     if(mapArray[y][x] == 1){
       $("#board").append("<div class='blue'></div>");
@@ -38,14 +39,48 @@ for (var y = 0; y < mapArray.length; y++) {
   }
 }
 //to create bubbles
+for (var i = 0; i > 0; i--) {
+  $(".arrow").append("<img src=images/arrow-image.png alt=arrow>")
+}
 
 
 
-// var arrowArray =[
-//   [arrow1,arrow2,arrow3,arrow4, arrow5]]
+// for (var i = 6; i > 0; i--) {
+// }
+// var mapArray = setInterval(arrow, 10000);
 //
-// var vectorArrow = document.getElementById('arrow1')
-// var vectorArrow = document.;
+// function mapArray() {
+//   var i = 6 == (mapArray[y][x] == 4)
+//   document.image = arrow
+// }
+
+  //--ar i = 0;
+  // var images = [];
+  // var time = 3000;
+  //
+  // images[0] = 'images/arrow-image.png';
+  //
+  // function changePosition() {
+  //   document.arrow.src = images[i];
+  //
+  //   if(i > images.length - 1){
+  //     i--;
+  //   }else{
+  //     i=0;
+  //   }
+  //   setTimeout('changePosition()', time);
+  // }
+  //
+  // window.offload = changePosition;
+
+// var myVar = setInterval(myArrow,3000);
+//
+// function myArrow() {
+//   var direction = new myArrow();
+//   document.getElementsByClassName('arrow').innerHTML
+// }
+
+
 
 
 
@@ -54,3 +89,20 @@ for (var y = 0; y < mapArray.length; y++) {
 //   // you need to devide by PI, and MULTIPLY by 180:
 //   var degrees = 180*angle/Math.PI;  //degrees
 //   return (360+Math.round(degrees))%360; //round number, avoid decimal fragments
+
+// var mapArray1 = [
+//   [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+//   [2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+//   [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+//   [2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+//   [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+//   [3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+//   [3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+//   [3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+//   [3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+//   [3,3,3,3,3,3,4,3,3,3,3,3,3,3],
+//   [3,3,3,3,3,3,3,3,3,3,3,3,3,3]
+// ];
+// set setInterval(function () {
+//
+// }, 10000);
